23b57aaa6c7f09616e37d70b3fbd4605560cb93a
Xicky <16Xicky@gmail.com>
2014-11-03 13:50:11 +0800

    UI is almost complete

--- des-ui.js
+++ des-ui.js
@@ -9,33 +9,41 @@ ARR_HEX2BIN = [
 
 function bin2hex(s) {
     var s2 = "", len = s.length;
+    for (var i = 0; i < len; i++) {
+        if (s[i] !== "0" && s[i] !== "1")
+            return "undefined";
+    }
     for (var i = 0; i < len; i += 4) {
         s2 += ARR_BIN2HEX[parseInt(s.slice(i, i+4), 2)];
     }
     return s2;
-};
+}
 function hex2bin(s) {
     var s2 = ""; len = s.length;
     for (var i = 0; i < len; i++) {
         s2 += ARR_HEX2BIN[s.charCodeAt(i)-48];
     }
     return s2;
-};
-
+}
 function bin_str2arr(s) {
     var arr = [], len = s.length;
     for (var i = 0; i < len; i++) {
         arr.push((s[i] === "0") ? 0 : 1);
     }
     return arr;
-};
+}
 function bin_arr2str(arr) {
     var s = "", len = arr.length;
     for (var i = 0; i < len; i++) {
         s += (arr[i] === 0) ? "0" : "1";
     }
     return s;
-};
+}
+
+function resizeText(text) {
+    text.style.height = 'auto';
+    text.style.height = text.scrollHeight-20+'px';
+}
 
 // Textarea auto-resize from http://stackoverflow.com/a/5346855/3816386
 var observe;
@@ -80,16 +88,32 @@ function isNotValid(text) {
     }
 };
 
+function updateKey() {
+    var key = document.getElementById("key");
+    var arrKey = bin_str2arr(key.value);
+    DES.generateKeys(arrKey);
+}
+
 function encrypt() {
-    var plain = document.getElementById("plain");
-    var cipher = document.getElementById("cipher");
-    
+    var arrPlain = bin_str2arr(plain.value);
+    var arrCipher = DES.encrypt(arrPlain);
+    var strCipher = bin_arr2str(arrCipher);
+    cipher.value = strCipher;
+    syncTextareas(cipher, cipherHex, cipherBinTip);
+    resizeText(cipher);
+    CIPHER_LEGAL = true;
 };
 function decrypt() {
-
+    var arrCipher = bin_str2arr(cipher.value);
+    var arrPlain = DES.decrypt(arrCipher);
+    var strPlain = bin_arr2str(arrPlain);
+    plain.value = strPlain;
+    syncTextareas(plain, plainHex, plainBinTip);
+    resizeText(plain);
+    PLAIN_LEGAL = true;
 };
 
-function bindInput(textarea, second, isHex, isPlain) {
+function bindInput(textarea, second, tip, isHex, which) {
     textarea.oninput = function() {
         var error, value, hexValue;
         if (isHex) {
@@ -102,27 +126,72 @@ function bindInput(textarea, second, isHex, isPlain) {
             second.value = hexValue;
         }
         if (error = isNotValid(value)) {
-            INPUT_LEGAL = false;
-            plain.className = "error";
-            plainBinTip.innerText = error;
+            if (!(INPUT_LEGAL & which)) // input was legal
+                INPUT_LEGAL ^= which;
+            switch(which) {
+                case INPUT_PLAIN:
+                    PLAIN_LEGAL = false;
+                    break;
+                case INPUT_CIPHER:
+                    CIPHER_LEGAL = false;
+                    break;
+                case INPUT_KEY:
+                    KEY_LEGAL = false;
+            }
+            textarea.className = "error";
+            second.className = "error";
+            // tip.innerText = error;
         } else {
-            INPUT_LEGAL = true;
-            plain.className = "";
-            third.innerText = "";
+            if (INPUT_LEGAL & which) // input was illegal
+                INPUT_LEGAL ^= which;
+            switch(which) {
+                case INPUT_PLAIN:
+                    PLAIN_LEGAL = true;
+                    break;
+                case INPUT_CIPHER:
+                    CIPHER_LEGAL = true;
+                    break;
+                case INPUT_KEY:
+                    KEY_LEGAL = true;
+            }
+            textarea.className = "";
+            second.className = "";
+            tip.innerText = "";
         }
     };
-    textarea.onchange = function() {
-        if (INPUT_LEGAL) {
-            if (isPlain) {
-                // TODO: encrypt();
-            } else {
-                // TODO: decrypt();
+    textarea.onblur = function() {
+        if (which & INPUT_PLAIN) {
+            if (KEY_LEGAL && PLAIN_LEGAL)
+                encrypt();
+        } else if (which & INPUT_KEY) {
+            if (KEY_LEGAL) {
+                updateKey();
+                if (PLAIN_LEGAL)
+                    encrypt();
+                else if (CIPHER_LEGAL)
+                    decrypt();
             }
+        } else {
+            if (KEY_LEGAL && CIPHER_LEGAL)
+                decrypt();
         }
     };
 };
 
-INPUT_LEGAL = false;
+function syncTextareas(text, hex, tip, ascii) {
+    hex.value = bin2hex(text.value);
+    text.className = "";
+    hex.className = "";
+    tip.innerText = "";
+}
+
+INPUT_LEGAL = 0;
+KEY_LEGAL = true;
+PLAIN_LEGAL = false;
+CIPHER_LEGAL = false;
+INPUT_PLAIN = 1;
+INPUT_CIPHER = 2;
+INPUT_KEY = 4;
 
 window.onload = function() {
     /* TODO:
@@ -131,37 +200,24 @@ window.onload = function() {
      *  DES.encrypt()
      *  Update textareas
      */
-    var plain = document.getElementById("plain");
-    var cipher = document.getElementById("cipher");
-    var plain_hex = document.getElementById("plain-hex");
-    var cipher_hex = document.getElementById("cipher-hex");
+    updateKey();
+    plain = document.getElementById("plain");
+    cipher = document.getElementById("cipher");
+    plainHex = document.getElementById("plain-hex");
+    cipherHex = document.getElementById("cipher-hex");
+    plainBinTip = document.getElementById("plain-bin-tip");
+    cipherBinTip = document.getElementById("cipher-bin-tip");
+    key = document.getElementById("key");
+    keyHex = document.getElementById("key-hex");
+    keyBinTip = document.getElementById("key-bin-tip");
+
     initTextareaResize(plain);
     initTextareaResize(cipher);
-    var plainBinTip = document.getElementById("plain-bin-tip");
-    plain.oninput = function() {
-        var error;
-        if (error = isNotValid(plain.value)) {
-            plain.className = "error";
-            plainBinTip.innerText = error;
-            plain_hex.value = bin2hex(plain.value);
-        }
-        else {
-            plain.className = "";
-            plainBinTip.innerText = "";
-        }
-    }
-    plain.onchange = function() {
-        var error;
-        if (error = isNotValid(plain.value)) {
-            plain.className = "error";
-            document.getElementById("plain-bin-tip").innerText = error;
-        }
-        else {
-            cipher.value = plain.value;
-        }
-    }
-    plain.onclick = function() {
-        plain.focus();
-        plain.select();
-    }
+
+    bindInput(plain, plainHex, plainBinTip, false, INPUT_PLAIN);
+    bindInput(plainHex, plain, plainBinTip, true, INPUT_PLAIN);
+    bindInput(cipher, cipherHex, cipherBinTip, false, INPUT_CIPHER);
+    bindInput(cipherHex, cipher, cipherBinTip, true, INPUT_CIPHER);
+    bindInput(key, keyHex, keyBinTip, false, INPUT_KEY);
+    bindInput(keyHex, key, keyBinTip, true, INPUT_KEY);
 };
